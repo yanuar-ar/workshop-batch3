874f5b9bb49b188f8fdc4ff4886521c93a3667e3
Yanzero <yanuar.ar@gmail.com>
2025-01-25 10:59:42 +0700

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint256 public number; 
+    uint256 public price;
+    address public owner; 
+
+    constructor() {
+        owner = msg.sender;
+        price = 100;
+    }
+
+    function setPrice(uint256 newPrice) public {
+        require(msg.sender == owner, "Only owner can set price");
+        price = newPrice;
+    }
+    
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner, "Only owner can set owner");
+        owner = newOwner;
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -7,9 +7,21 @@ import {Counter} from "../src/Counter.sol";
 contract CounterTest is Test {
     Counter public counter;
 
+    address public alice = makeAddr("alice");
+
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+    }
+
+    function test_SetPrice() public {
+        counter.setPrice(100);
+        assertEq(counter.price(), 100, "price should be 1000");
+        console.log("price", counter.price());
+
+        vm.prank(alice);
+        vm.expectRevert("Only owner can set price");
+        counter.setPrice(200);
     }
 
     function test_Increment() public {
